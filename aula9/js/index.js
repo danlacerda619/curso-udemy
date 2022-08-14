2fa7925bb64e5fa745ebaa207eb4fd3db89b8505
dan619 <danilolacerda619@gmail.com>
2022-08-14 20:26:59 -0300

    exercício

---
+++ aula9/js/index.js
@@ -0,0 +1,7 @@
+let num1 = prompt('digite um número')
+let num2 = prompt('digite outro número')
+num1 = Number(num1)
+num2 = Number(num2)
+console.log(num1 + num2)
+const resultado = num1 + num2
+alert (`o resultado foi: ${resultado}`)
