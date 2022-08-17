0896a168335209d10ed1bd60a259671b6aad34de
dan619 <danilolacerda619@gmail.com>
2022-08-17 02:25:11 -0300

    Math

---
+++ aula12/index.js
@@ -0,0 +1,16 @@
+// Math.floor arredonda o número para baixo
+// Math.ceil arrendonda o número para cima
+// Math.rounf arredonda para o mais proximo e na metade arredonda pra cima
+// Math.max achar o maior número numa sequencia de números 
+// Math.min para achar o menor número de uma sequencia
+// Math.random criar um numero aleatorio com casas decimais 
+//
+
+
+//let num1 = 10.9554
+//let num2 = Math.ceil(num1)
+//console.log(num2)
+
+let num1 = 10.5676
+let num2 = Math.floor(num1)
+console.log(num2)
