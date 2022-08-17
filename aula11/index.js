3b9bc29d2efaf98108a3d105db64bb8fccd3b897
dan619 <danilolacerda619@gmail.com>
2022-08-17 01:04:59 -0300

    numbers

---
+++ aula11/index.js
@@ -0,0 +1,27 @@
+// toFixed para "arredodar" numeros decimais
+// isInterger para saber se o número é inteiro
+// isNaN para retorar se a conta é ou não um NaN = not a number
+//  toString converter para string sem alterar a variável
+
+
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+
+// num1 = ((num1 * 100) + (num2 * 100)) / 100; 
+// num1 = ((num1 * 100) + (num2 * 100)) / 100;
+// num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+console.log(num1.toFixed(2));
+console.log(Number.isInteger(num1))
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// console.log(Number.isNaN(temp));
+// let temp = num1 + '5';
+// console.log(temp);
+ 
