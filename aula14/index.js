6f3e022aa37ee4ede66ba6cb1a22dcc51c74c239
dan619 <danilolacerda619@gmail.com>
2022-08-18 03:19:29 -0300

    arrays

---
+++ aula14/index.js
@@ -0,0 +1,33 @@
+              //0     //1     //2
+let alunos = ['Luiz', 'Maria', 'João']
+ 
+//alunos.pop() //elimina o ultimo
+//console.log(alunos)
+
+
+//delete alunos[2]; // deletar um elemento sem mexer no indice
+//console.log(alunos)  porem irá ficar um buraco vazio no array
+
+
+//alunos.slice(0, -1);
+//console.log(alunos)
+
+//alunos.unshift('ronaldinho'); // adiciona no começo
+//alunos.unshift('kaká');
+//console.log(alunos)
+
+//alunos.push('laura'); // adiciona no fim
+ //console.log(alunos)
+
+//alunos[alunos.length] = 'luiza'; //adiciona no fim
+ //console.log(alunos)
+
+//alunos[0] = 'danilo'; // altera
+//alunos[3] = 'victor'; // adciona
+ //console.log(alunos[3])
+
+//console.log(alunos);
+//console.log(alunos[2]);
+//console.log(alunos[0])
+
+
