8586b6bb945fc142b7c17fb4404559cab9c462e5
dan619 <danilolacerda619@gmail.com>
2022-08-18 02:40:10 -0300

    exercício numbers e maths

---
+++ aula13/ex/js/script.js
@@ -0,0 +1,12 @@
+const numero = prompt('Digite um número:');
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
+texto.innerHTML += `<p> é NaN: ${Number.isNaN(numero)}</p>`
+texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(numero)}</p>`
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p> `
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p> ` 
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p> `
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
